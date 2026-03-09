6a61b9889d90020b0f379b36d27754aa24c98f84
japhet john <japhetjohnk@gmail.com>
2026-03-09 20:11:47 +0000

    deleted files

--- backend/check_balance_script.js
+++
@@ -1,7 +0,0 @@
-const { Connection, PublicKey, clusterApiUrl, LAMPORTS_PER_SOL } = require('@solana/web3.js');
-async function run() {
-    const connection = new Connection(clusterApiUrl('mainnet-beta'), 'confirmed');
-    const bal = await connection.getBalance(new PublicKey('Gy2n4pZePk1vZyx76GtNERaLSgcoQYD2MnohMxFWbtCV'));
-    console.log(`Gas Sponsor Wallet Balance: ${bal / LAMPORTS_PER_SOL} SOL`);
-}
-run();
